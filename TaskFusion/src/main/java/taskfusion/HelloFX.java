b078391c829c7504c859a3c68ea1d2454291f9e9
Rasmus Wiuff <rwiuff@gmail.com>
2023-03-24 10:44:08 +0100

    Changed HelloFX.java

--- TaskFusion/src/main/java/taskfusion/HelloFX.java
+++ TaskFusion/src/main/java/taskfusion/HelloFX.java
@@ -14,7 +14,7 @@ public class HelloFX extends Application {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        Scene scene = new Scene(new StackPane(l), 1000, 1000);
         stage.setScene(scene);
         stage.show();
     }
