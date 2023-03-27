241fdc630f13032b1157843a2627a13cc0ee4168
FluxEntity365 <s200747@student.dtu.dk>
2023-03-27 12:02:53 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:TaskFusion/src/main/java/taskfusion/TaskFusion.java

--- TaskFusion/src/main/java/taskfusion/TaskFusion.java
+++ TaskFusion/src/main/java/taskfusion/TaskFusion.java
@@ -4,6 +4,10 @@ public class TaskFusion {
 
     public static void main(String[] args) {
         System.out.print("Hello World");
+        System.out.println("Happy mob programming! Try type: mob moo");
     }
+
+        
+
 
 }
