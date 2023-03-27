ed87fc48311010850a42c50c580724d6daae664d
FluxEntity365 <s200747@student.dtu.dk>
2023-03-27 12:10:57 +0200

    Squashed commit of the following:
    commit 241fdc630f13032b1157843a2627a13cc0ee4168
    Author: FluxEntity365 <s200747@student.dtu.dk>
    Date:   Mon Mar 27 12:02:53 2023 +0200
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:TaskFusion/src/main/java/taskfusion/TaskFusion.java
    
    commit 9fe653e38f083858c9b8cce30ed32bf63c1c4dc9
    Author: FluxEntity365 <s200747@student.dtu.dk>
    Date:   Mon Mar 27 12:00:33 2023 +0200
    
        mob start [ci-skip] [ci skip] [skip ci]

--- TaskFusion/src/main/java/taskfusion/TaskFusion.java
+++ TaskFusion/src/main/java/taskfusion/TaskFusion.java
@@ -4,6 +4,7 @@ public class TaskFusion {
 
     public static void main(String[] args) {
         System.out.print("Hello World");
+        System.out.println("Happy mob programming! Try type: mob moo");
     }
 
 }
