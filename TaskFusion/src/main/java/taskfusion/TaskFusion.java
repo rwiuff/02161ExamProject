5375f87661c55f4f9f14880a9622c106e39004ea
FluxEntity365 <s200747@student.dtu.dk>
2023-03-27 14:57:38 +0200

    test

--- TaskFusion/src/main/java/taskfusion/TaskFusion.java
+++ TaskFusion/src/main/java/taskfusion/TaskFusion.java
@@ -4,7 +4,7 @@ public class TaskFusion {
 
     public static void main(String[] args) {
         System.out.print("Hello World");
-        System.out.println("Happy mob programming! Try type: mob moo");
+        System.out.println("Happy mob programming! Try type: mob moo!");
     }
 
 }
