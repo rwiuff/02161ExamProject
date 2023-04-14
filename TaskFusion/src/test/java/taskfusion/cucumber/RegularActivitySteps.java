faa807ea9ff7e33ebfd4199447bae08d4161700e
Kasper Sylvest <kasper@kaspers.dk>
2023-04-14 22:17:46 +0200

    refactor regualr act steps

--- TaskFusion/src/test/java/taskfusion/cucumber/RegularActivitySteps.java
+++ TaskFusion/src/test/java/taskfusion/cucumber/RegularActivitySteps.java
@@ -16,7 +16,7 @@ public class RegularActivitySteps {
   @When("the user creates the regular activity {string} with start week {int} and end week {int}")
   public void theUserCreatesTheRegularActivityWithStartWeekAndEndWeek(String string, Integer int1, Integer int2) {
     try {
-      this.taskFusion.createRegularActivity(string, int1, int2);
+      this.taskFusion.getEmployeeFacade().createRegularActivity(string, int1, int2);
     } catch (Exception e) {
       this.errorMessageHolder.setErrorMessage(e.getMessage());
     }
@@ -24,13 +24,13 @@ public class RegularActivitySteps {
 
   @Then("the user has a regular activity with title {string} with start week {int} and end week {int}")
   public void theUserHasARegularActivityWithTitleWithStartWeekAndEndWeek(String string, Integer int1, Integer int2) {
-    this.taskFusion.hasRegularActivity(string, int1, int2);
+    this.taskFusion.getEmployeeFacade().hasRegularActivity(string, int1, int2);
   }
 
   @When("the user creates the regular activity {string} with start week {string} and end week {int}")
   public void theUserCreatesTheRegularActivityWithStartWeekAndEndWeek(String string, String string2, Integer int1) {
     try {
-      this.taskFusion.createRegularActivity(string, null, int1);
+      this.taskFusion.getEmployeeFacade().createRegularActivity(string, null, int1);
       ;
     } catch (Exception e) {
       this.errorMessageHolder.setErrorMessage(e.getMessage());
@@ -40,7 +40,7 @@ public class RegularActivitySteps {
   @When("the user creates the regular activity {string} with start week {int} and end week {string}")
   public void theUserCreatesTheRegularActivityWithStartWeekAndEndWeek(String string, Integer int1, String string2) {
     try {
-      this.taskFusion.createRegularActivity(string, int1, null);
+      this.taskFusion.getEmployeeFacade().createRegularActivity(string, int1, null);
     } catch (Exception e) {
       this.errorMessageHolder.setErrorMessage(e.getMessage());
     }
